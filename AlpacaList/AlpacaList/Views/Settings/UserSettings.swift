954380a520f5cd432ca672c24ddbb2e5dffd799b
alpaca <alpaca@vision.net>
2023-07-09 21:54:55 -0700

    WIP refactor settings to use lists

--- AlpacaList/AlpacaList/Views/Settings/UserSettings.swift
+++ AlpacaList/AlpacaList/Views/Settings/UserSettings.swift
@@ -15,41 +15,24 @@ let fakeUsers = [
     "serendipity@eternalhorizon.co"
 ]
 
-struct UserSettingsItemCheckMark: View {
-    var isChecked: Bool
-    
-    var body: some View {
-        // return a puruple checkmark in circle if checked
-        // return a gray cicrle sybmol if not checked
-        if isChecked {
-            Image(systemName: "checkmark.circle.fill")
-                .foregroundColor(.purple)
-                .fontWeight(.bold)
-        } else {
-            Image(systemName: "circle")
-                .foregroundColor(Color.primary.opacity(0.35))
-                .fontWeight(.light)
-        }
-    }
-}
 
-struct UserSettingsItem: View {
-    var username: String
-    var isChecked: Bool
+
+struct UserSettingsAddUserButton: View {
+    var action: () -> Void
     
     var body: some View {
-        Button(action: {}) {
+        SettingsButton(action: action) {
             HStack {
                 // bookmark image
-                UserSettingsItemCheckMark(isChecked: isChecked)
+                Image(systemName: "plus")
                     .font(.system(size: 20))
+                    .fontWeight(.bold)
                     .frame(width: 20)
                 Spacer().frame(width: 20)
-                Text(username)
+                Text("Add New User")
                     .font(.system(size: 18))
-                    .lineLimit(1)
                     .fontWeight(.medium)
-                    .foregroundColor(.primary)
+                    .lineLimit(1)
                 Spacer()
             }
             .padding(5)
@@ -57,82 +40,32 @@ struct UserSettingsItem: View {
     }
 }
 
-struct UserSettingsAddUserButton: View {
-    // add a tap gesture that handles the start / end events
-    var gesture: some Gesture {
-        return DragGesture(minimumDistance: 0)
-            .onChanged { _ in
-                print("started")
-            }
-            .onEnded { _ in
-                print("ended")
-            }
-    }
-    
-    var body: some View {
-        HStack {
-            // bookmark image
-            Image(systemName: "plus")
-                .font(.system(size: 20))
-                .fontWeight(.bold)
-                .frame(width: 20)
-            Spacer().frame(width: 20)
-            Text("Add New User")
-                .font(.system(size: 18))
-                .fontWeight(.medium)
-                .lineLimit(1)
-            Spacer()
-        }
-        .padding(5)
-        .gesture(gesture)
-    }
-}
-
-struct SettingsHeader: View {
-    var title: String
-    
-    var body: some View {
-        Text(title)
-            .font(.system(size: 16))
-            .fontWeight(.semibold)
-            .fontDesign(.monospaced)
-            .foregroundColor(Color.primary.opacity(0.5))
-    }
-}
-
 struct UserSettings: View {
     @State var selectedUser: String?
     
     var body: some View {
-        List {
-            Section(header: SettingsHeader(title: "Change User")) {
+        SettingsList {
+            SettingsSection(title: "Change User") {
                 ForEach(fakeUsers, id: \.self) { user in
-                    UserSettingsItem(username: user,
-                                     isChecked: user == selectedUser)
-                    .gesture(TapGesture()
-                                .onEnded { _ in
-                                    selectedUser = user
-                                })
+                    SettingsRadioItem(
+                        username: user,
+                        isChecked: user == selectedUser,
+                        action: {
+                            selectedUser = user
+                        }
+                    )
                 }
-                UserSettingsItem(username: "Anonymous User", isChecked: selectedUser == nil)
-                    .gesture(TapGesture()
-                        .onEnded { _ in
-                            selectedUser = nil
-                        })
-                UserSettingsAddUserButton()
-                    .simultaneousGesture(TapGesture()
-                        .onEnded { _ in
-                            print("new")
-                        })
+                SettingsRadioItem(
+                    username: "Anonymous User",
+                    isChecked: selectedUser == nil,
+                    action: {
+                        selectedUser = nil
+                    }
+                
+                )
+                UserSettingsAddUserButton(action: { print("meow")})
             }
-            .listRowBackground(
-                Color.clear.background(.thinMaterial)
-            )
-            .listRowInsets(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
         }
-        .scrollContentBackground(.hidden)
-        .listStyle(InsetGroupedListStyle())
-        .background(.ultraThinMaterial)
     }
 }
 
